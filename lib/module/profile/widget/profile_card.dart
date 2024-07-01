022239eeeae5423f1052eff009f027ab1edcf210
denyocr93 <flutterlabz@gmail.com>
2024-07-01 15:16:20 +0700

    .

--- lib/module/profile/widget/profile_card.dart
+++ lib/module/profile/widget/profile_card.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
 import 'package:hyper_ui/module/pencatatan_keuangan/dashboard/controller/dashboard_controller.dart';
