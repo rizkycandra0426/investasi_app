e9d6a8887875572c427e0126bb04c5c5cc39f139
denyocr93 <flutterlabz@gmail.com>
2024-07-09 11:29:04 +0700

    .

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 String get baseUrl {
   // return "https://investasi.9code.id/api";
-  return "http://192.168.173.168/api";
+  return "http://192.168.173.168:8000/api";
   // return "http://skripsi-investasi.my.id/api";
   // return "http://192.168.47.168:8000/api";
 }
