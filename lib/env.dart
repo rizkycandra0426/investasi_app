969e2952d1cd168ba9d660397c1c97bf1041dcdd
denyocr93 <flutterlabz@gmail.com>
2024-10-23 08:30:44 +0700

    .

--- lib/env.dart
+++ lib/env.dart
@@ -2,11 +2,11 @@
 var vpsUrl = "http://8.219.73.201/api";
 String get baseUrl {
   return vpsUrl;
-  return "http://192.168.100.5:8000/api";
-  return "http://103.23.199.220:8000/api";
-  return "https://smartfinance-backend.inovasi-digital.my.id/api";
-  return "http://192.168.100.5:8000/api";
-  return "http://192.168.124.168:8000/api";
+  // return "http://192.168.100.5:8000/api";
+  // return "http://103.23.199.220:8000/api";
+  // return "https://smartfinance-backend.inovasi-digital.my.id/api";
+  // return "http://192.168.100.5:8000/api";
+  // return "http://192.168.124.168:8000/api";
   // return "http://192.168.100.5:8000/api";
   // return "http://157.230.253.157:8000/api";
 
